bd01ab83d8022f14e14c30072a26171af0e3f3d5
SPSINGH3011 <spsingh302212@gmail.com>
2025-08-28 01:05:59 +0530

    co

--- root_module/dev-env/provider.tf
+++ root_module/dev-env/provider.tf
@@ -4,6 +4,8 @@ terraform {
         storage_account_name = "backendsta"
         container_name = "backendcontainer"
         key = "dev.tfstate" 
+     subscription_id       = "81cc1915-8d88-419e-8fa3-0178811761bd"
+    use_azuread_auth      = true
     }
   required_providers {
     azurerm = {
